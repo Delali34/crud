87453ca9bef5a23163980bb751f596d1629465b1
Dela <ernestanyomitse752@gmail.com>
2022-09-06 10:32:25 -1200

    crud

---
+++ src/Reducer/Reducer.js
@@ -0,0 +1,26 @@
+let initialStore = {users:[
+    {
+      name: " Dela",
+      number: 5,
+      location: " Danfa",
+      id: "787y887chukjchho3fw",
+    },
+    {
+      name: " Ernest",
+      number: 21,
+      location: "Danfa",
+      id: "bvkhjvhvjih939u494",
+    },
+    {
+      name: " NanaAwoa",
+      number: 18,
+      location: "Danfa",
+      id: "hhuchieih939u4654",
+    },
+  ]};
+
+const Reducer = (state = initialStore, action) => {
+    return state;
+}
+
+export default Reducer
---
+++ src/store/store.js
@@ -0,0 +1,21 @@
+import { legacy_createStore as createStore} from "redux";
+import {Reducer} from "..Reducer/Reducer";
+
+let Store = createStore(Reducer)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default Store
